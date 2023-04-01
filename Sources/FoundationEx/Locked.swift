8a79abfd65afd7c6340f617c9a17a1aa9be35534
Paul Calnan <pwc3@users.noreply.github.com>
2023-03-31 20:19:36 -0400

    Added license

--- Sources/FoundationEx/Locked.swift
+++ Sources/FoundationEx/Locked.swift
@@ -1,9 +1,23 @@
 //
-//  Locked.swift
-//  Rocket Insights
+// Copyright (c) 2023 DEPT Digital Products, Inc.
 //
-//  Created by Ilya Belenkiy on 8/21/20.
-//  Copyright © 2021 Rocket Insights. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
 import Foundation
@@ -11,7 +25,7 @@ import Foundation
 public final class Locked<A> {
     private let queue: DispatchQueue
     private var _value: A
-    
+
     public init(_ value: A, queueLabel: String = "") {
         queue = DispatchQueue(label: queueLabel)
         self._value = value
--- Sources/FoundationEx/Serialization/DefaultsValue.swift
+++ Sources/FoundationEx/Serialization/DefaultsValue.swift
@@ -1,9 +1,23 @@
 //
-//  DefaultsValue.swift
-//  Rocket Insights
+// Copyright (c) 2023 DEPT Digital Products, Inc.
 //
-//  Created by Ilya Belenkiy on 9/14/22.
-//  Copyright © 2022 Rocket Insights. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
 import Foundation
@@ -32,7 +46,7 @@ public extension DefaultsRawVAlue {
     var rawDefaultsValue: Self? {
         self
     }
-    
+
     static func defaultsValue(_ rawValue: RawDefaultsValue) -> Self? {
         return rawValue
     }
@@ -45,7 +59,7 @@ public extension DefaultsJSONValue {
     var rawDefaultsValue: Data? {
         maybe { try JSONEncoder().encode(self) }
     }
-    
+
     static func defaultsValue(_ rawValue: Data) -> Self? {
         maybe { try JSONDecoder().decode(Self.self, from: rawValue) }
     }
@@ -56,7 +70,7 @@ public extension UserDefaults {
         guard let rawValue = value(forKey: key) as? T.RawDefaultsValue else { return nil }
         return T.defaultsValue(rawValue)
     }
-    
+
     func encode<T: DefaultsValue>(_ value: T, forKey key: String) {
         let savedValue: T? = decode(key)
         if value != savedValue {
